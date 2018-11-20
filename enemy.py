32e3fa5cea035486ffa6db6cef05b9a98bad826b
inazumaron <30858153+inazumaron@users.noreply.github.com>
2018-11-20 19:54:28 +0700

    Updated enemy.py
    added move method
    changed default starting position

--- enemy.py
+++ enemy.py
@@ -2,9 +2,16 @@ import pyglet
 
 class Enemy_1(object):
 	"""docstring for Enemy_1"""
-	def __init__(self, arg):
+	def __init__(self):		#
 		super(Enemy_1, self).__init__()
+		self.x = 0
+		self.y = 600
 		self.life = 100
 		self.attack = "normal"
 		self.damage = 10
 		self.cooldown = 10 #Rate of fire
+		self.id = "easy_1"
+
+	def move(self):		#AI of enemy
+		self.x += 10 	#Move right till out of screen (600x800)
+		return self
