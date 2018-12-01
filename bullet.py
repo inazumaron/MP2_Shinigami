cf9c34bdb4abaa072c54955229cd2b5985d6a19f
inazumaron <skuyanon@gmail.com>
2018-12-01 21:19:00 +0700

    stabilized homing

--- bullet.py
+++ bullet.py
@@ -8,27 +8,21 @@ def bullet_action(bullet,target_list,time_elapse):
 	bullet.obj_vy += bullet.obj_ay
 	#Update bullet location
 	#Just add more behavior like if bullet is homing etc
-	if bullet.modifiers["homing"]:
+	if bullet.modifiers["homing"] and len(target_list)>0:
 		closest = target_list[0]
 		distance = col.get_distance(bullet.obj_x,bullet.obj_y,target_list[0].x,target_list[0].y)
-		for t in (1,len(target_list)):
-			temp = col.get_distance(bullet.obj_x,bullet.obj_y,target_list[t].x,target_list[t].y)
+		for t in target_list:
+			temp = col.get_distance(bullet.obj_x,bullet.obj_y,t.x,t.y)
 			if temp < distance and temp < 400:
 				distance = temp
-				closest = target_list[t]
-		if closest.x < bullet.obj_x:
-			bullet.obj_ax -= 50
+				closest = t
+		if abs(closest.x-bullet.obj_x) < 20:
+			bullet.obj_vx = 0
+		elif closest.x < bullet.obj_x:
+			bullet.obj_vx -= 1
 		elif closest.x > bullet.obj_x:
-			bullet.obj_ax += 50
-		else:
-			bullet.obj_ax = 0
-		#Do stuff here
-		#find closest enemy within a 
-		#certain distance from the list target_list (may be enemy or player)
-	#Also check if in collision with target
-	#if in collision:
-	#return bullet,target
-	#else:
+			bullet.obj_vx += 1
+
 	if bullet.obj_y > 800:
 		bullet.destroy = True
 	try:
