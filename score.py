da5dd2bba9eebb183558b741150c466693170eb9
inazumaron <skuyanon@gmail.com>
2018-12-05 19:25:13 +0700

    commited

--- score.py
+++ score.py
@@ -3,19 +3,18 @@ filepath =  os.path.normpath('resc/scores.txt')
 
 def get_scores():
 	#get scores from file
-	scores = {"name":0} #format must be like this (0 is the score)
-	scores = dict()
-	with open(filepath) as fp:
-        	line = fp.readline().strip()
-        	while line:
-        		temp = line.split(':')
-        		scores[temp[0]] = int(temp[1])
-        		line = fp.readline().strip()
-	#scores = {"name":0} #format must be like this (0 is the score)
+	scores = list()
+	with open(filepath) as file:
+		line = file.readline().strip()
+		while line:
+			temp = int(line)
+			scores.append(temp)
+			line = file.readline().strip()
 	return scores
 
-def add_score(name, score):
+def add_score(score):
 	#add to file
+<<<<<<< HEAD
 	name = input("Input your name: ")
 	scores = get_scores()
 	scores[name]=score
@@ -28,3 +27,25 @@ def add_score(name, score):
 		#writes back to file
 		file.add(data)
 	scores[name]=score
+=======
+	data = list()
+	with open(filepath, 'r') as file:
+		# read a list of lines into data
+		line = file.readline().strip()
+		while line:
+			temp = int(line)
+			data.append(temp)
+			line = file.readline().strip()
+	if score > 0:
+		data.append(score)
+	data.sort(reverse = True)
+	if len(data) > 10:
+		data = data[:10]
+	scores = ''
+	for n in data:
+		scores += str(n) + '\n'
+	scores.strip()
+	with open(filepath, 'w') as file:
+		#writes back to file
+		file.writelines(scores)
+>>>>>>> 0ea248cd2325bd22d4840455da76482aa8b79f64
