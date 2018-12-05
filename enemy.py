f9595c9afa43454fda9c6419f51e2454452c00e5
eleanor810 <45189940+eleanor810@users.noreply.github.com>
2018-12-05 20:07:10 +0800

    Update enemy.py
    merges enemies 0 a-e into just one class. also, makes enemies 1,2,3,4,5, and 8 more versatile by letting them take a desired x-coordinate. 
    Also implements changes so that enemies 0,1,2,3,4 are easy, enemies 5,6,7 are medium and enemies 8,9,10 are hard.

--- enemy.py
+++ enemy.py
@@ -3,15 +3,15 @@ import bullet as bull
 import random
 
 
-class Enemy_0a(object):
+class Enemy_0(object):
 	"""Moves down."""
-	def __init__(self, time):
-		super(Enemy_0a, self).__init__()
-		self.x = 50
+	def __init__(self, time, x):
+		super(Enemy_0, self).__init__()
+		self.x = x
 		self.y = 800
 		self.life = 20
 		self.attack = 'normal'
-		self.damage = 10
+		self.damage = 5
 		self.cooldown = 10
 		self.id = 'easy_0'
 		self.points = 5
@@ -38,147 +38,11 @@ class Enemy_0a(object):
 		else:
 			return None	
 
-class Enemy_0b(object):
-	def __init__(self, time):
-		super(Enemy_0b, self).__init__()
-		self.x = 175
-		self.y = 800
-		self.life = 20
-		self.attack = 'normal'
-		self.damage = 10
-		self.cooldown = 10
-		self.id = 'easy_0'
-		self.points = 5
-
-	def move(self):
-		self.y -= 2
-		return self
-
-	def shoot(self, time, player_point):
-		if time%30 == 0 and time <= 1200:					
-			bullet = bull.bullet()
-			bullet.obj_x = self.x 			
-			bullet.obj_y = self.y 			
-			bullet.obj_vy = -10				
-			bullet.obj_vx = 0
-			return bullet
-		elif time >= 1200 and time%20 == 0:
-			bullet = bull.bullet()
-			bullet.obj_x = self.x 			###Initial position
-			bullet.obj_y = self.y 			
-			bullet.obj_vy = -10				###Initial velocity
-			bullet.obj_vx = 0
-			return bullet	
-		else:
-			return None
-
-class Enemy_0c(object):
-	def __init__(self, time):
-		super(Enemy_0c, self).__init__()
-		self.x = 300
-		self.y = 800
-		self.life = 20
-		self.attack = 'normal'
-		self.damage = 10
-		self.cooldown = 10
-		self.id = 'easy_0'
-		self.points = 5
-
-	def move(self):
-		self.y -= 2
-		return self
-
-	def shoot(self, time, player_point):
-		if time%30 == 0 and time <= 1200:					###Shoots bullets every 1/3 of a second.
-			bullet = bull.bullet()
-			bullet.obj_x = self.x 			###Initial position
-			bullet.obj_y = self.y 			
-			bullet.obj_vy = -10				###Initial velocity
-			bullet.obj_vx = 0
-			return bullet
-		elif time >= 1200 and time%20 == 0:
-			bullet = bull.bullet()
-			bullet.obj_x = self.x 			###Initial position
-			bullet.obj_y = self.y 			
-			bullet.obj_vy = -10				###Initial velocity
-			bullet.obj_vx = 0
-			return bullet	
-		else:
-			return None	
-
-class Enemy_0d(object):
-	def __init__(self, time):
-		super(Enemy_0d, self).__init__()
-		self.x = 425
-		self.y = 800
-		self.life = 20
-		self.attack = 'normal'
-		self.damage = 10
-		self.cooldown = 10
-		self.id = 'easy_0'
-		self.points = 5
-
-	def move(self):
-		self.y -= 2
-		return self
-
-	def shoot(self, time, player_point):
-		if time%30 == 0 and time <= 1200:					###Shoots bullets every 1/3 of a second.
-			bullet = bull.bullet()
-			bullet.obj_x = self.x 			###Initial position
-			bullet.obj_y = self.y 			
-			bullet.obj_vy = -10				###Initial velocity
-			bullet.obj_vx = 0
-			return bullet
-		elif time >= 1200 and time%20 == 0:
-			bullet = bull.bullet()
-			bullet.obj_x = self.x 			###Initial position
-			bullet.obj_y = self.y 			
-			bullet.obj_vy = -10				###Initial velocity
-			bullet.obj_vx = 0
-			return bullet	
-		else:
-			return None
-
-class Enemy_0e(object):
-	def __init__(self, time):
-		super(Enemy_0e, self).__init__()
-		self.x = 550
-		self.y = 800
-		self.life = 20
-		self.attack = 'normal'
-		self.damage = 10
-		self.cooldown = 10
-		self.id = 'easy_0'
-		self.points = 5
-
-	def move(self):
-		self.y -= 2
-		return self
-
-	def shoot(self, time, player_point):
-		if time%30 == 0 and time <= 1200:					###Shoots bullets every 1/3 of a second.
-			bullet = bull.bullet()
-			bullet.obj_x = self.x 			###Initial position
-			bullet.obj_y = self.y 			
-			bullet.obj_vy = -10				###Initial velocity
-			bullet.obj_vx = 0
-			return bullet
-		elif time >= 1200 and time%20 == 0:
-			bullet = bull.bullet()
-			bullet.obj_x = self.x 			###Initial position
-			bullet.obj_y = self.y 			
-			bullet.obj_vy = -10				###Initial velocity
-			bullet.obj_vx = 0
-			return bullet	
-		else:
-			return None
-
 class Enemy_1(object):
 	"""Moves downwards on the left side of the screen."""
-	def __init__(self, time):		
+	def __init__(self, time,x):		
 		super(Enemy_1, self).__init__()
-		self.x = 50							###Initial position
+		self.x = x							###Initial position
 		self.y = 830						
 		self.life = 40						###Health
 		self.attack = "normal"				###Attack
@@ -190,7 +54,7 @@ class Enemy_1(object):
 		self.points = 5						#Points
 
 
-	def move(self, time):		
+	def move(self):		
 		self.y -= 3							###Velocity
 		return self						
 
@@ -206,11 +70,12 @@ class Enemy_1(object):
 			return None	
 
 class Enemy_2(object):
-	'''Moves on a square pattern at the top half of the screen counter-clockwise.'''
-	def __init__(self, time):		
+	'''Moves on a square pattern at the top half of the screen., either clockwise or
+		counter-clockwise depending on the starting point.'''
+	def __init__(self, time, x):		
 		super(Enemy_2, self).__init__()
-		self.x = 50
-		self.y = 850
+		self.x = x
+		self.y = 800
 		self.life = 60
 		self.attack = "normal"
 		self.damage = 10
@@ -219,25 +84,29 @@ class Enemy_2(object):
 		self.right = True
 		self.time = time
 		self.points = 8
+		self.direction = 'counter-clockwise'
+		if x >= 300:
+			self.direction = 'clockwise'
 
-
-	def move(self, time):
-		if self.x == 50 and self.y > 250:		###Moves downwards
-			self.left = False
-			self.down = True
-			self.y -= 2.5
-		if self.x < 550 and self.y == 250:		###Moves to the right
-			self.down = False
-			self.right = True
-			self.x += 2.5
-		if self.x == 550 and self.y < 750:		###Moves upwards
-			self.right = False
-			self.up = True
-			self.y += 2.5
-		elif self.x > 50 and self.y == 750:		###Moves to the left
-			self.up = False
-			self.left = True
-			self.x -= 2.5
+	def move(self):
+		if self.direction == 'counter-clockwise':
+			if self.x == 50 and self.y > 250:		###Moves downwards
+				self.y -= 2.5
+			if self.x < 550 and self.y == 250:		###Moves to the right
+				self.x += 2.5
+			if self.x == 550 and self.y < 750:		###Moves upwards
+				self.y += 2.5
+			elif self.x > 50 and self.y == 750:		###Moves to the left
+				self.x -= 2.5
+		elif self.direction == 'clockwise':
+			if self.x == 550 and self.y > 250: ###moves downwards
+				self.y -= 2.5					
+			if self.x > 50 and self.y == 250:		###Moves to the left
+				self.x -= 2.5
+			if self.x == 50 and self.y < 750:		###Moves upwards
+				self.y += 2.5
+			elif self.x < 550 and self.y == 750:		###Moves to the right
+				self.x += 2.5
 		return self
 
 	def shoot(self, time, player_point):
@@ -252,12 +121,12 @@ class Enemy_2(object):
 			return None		
 
 class Enemy_3(object):
-	'''Moves upwards to the left.''' 
-	def __init__(self, time):		
+	'''Moves upwards in a diagonal direction.''' 
+	def __init__(self, time, x):		
 		super(Enemy_3, self).__init__()
-		self.x = 630
-		self.y = 200
-		self.life = 80
+		self.x = x
+		self.y = 300
+		self.life = 70
 		self.attack = "normal"
 		self.damage = 10
 		self.cooldown = 10 
@@ -265,11 +134,18 @@ class Enemy_3(object):
 		self.left = True
 		self.time = time
 		self.points = 12
+		self.direction = 'right'
+		if x >= 300:
+			self.direction = 'left'
 
 
 	def move(self):	
-		self.y += 4			
-		self.x -= 3
+		if self.direction == 'left':
+			self.y += 4			
+			self.x -= 3
+		elif self.direction == 'right':
+			self.y += 4
+			self.x += 3
 		return self
 
 	def shoot(self, time, player_point):
@@ -284,24 +160,30 @@ class Enemy_3(object):
 			return None	
 
 class Enemy_4(object):
-	'''Moves upwards to the right.'''
-	def __init__(self, time):		
+	'''Moves upwards in a diagonal direction. Has two guns''' 
+	def __init__(self, time, x):		
 		super(Enemy_4, self).__init__()
-		self.x = -30
-		self.y = 200
+		self.x = x
+		self.y = 400
 		self.life = 80
 		self.attack = "normal"
 		self.damage = 10
-		self.cooldown = 10
-		self.id = "easy_3"
+		self.cooldown = 15
+		self.id = "easy_4"
 		self.right = True
 		self.time = time
 		self.points = 20
-
+		self.direction = 'right'
+		if x >= 300:
+			self.direction = 'left'
 
 	def move(self):	
-		self.y += 4
-		self.x += 3
+		if self.direction == 'left':
+			self.y += 4			
+			self.x -= 3
+		elif self.direction == 'right':
+			self.y += 4
+			self.x += 3
 		return self
 
 	def shoot(self, time, player_point):
@@ -316,22 +198,22 @@ class Enemy_4(object):
 			return None	
 
 class Enemy_5(object):
-	'''Moves downwards on the right side of the screen.'''
-	def __init__(self, time):		#
+	'''Moves downwards.'''
+	def __init__(self, time, x,y):		#
 		super(Enemy_5, self).__init__()
-		self.x = 550
-		self.y = 830
-		self.life = 40
+		self.x = x
+		self.y = y
+		self.life = 100
 		self.attack = "normal"
 		self.damage = 10
-		self.cooldown = 10
+		self.cooldown = 20
 		self.id = "med_1"
 		self.right = True
 		self.time = time
 		self.points = 30
 
 
-	def move(self, time):			
+	def move(self):			
 		self.y -= 3					
 		return self	
 
@@ -360,7 +242,7 @@ class Enemy_6(object):
 		self.right = True
 		self.points = 40
 
-	def move(self,time):		
+	def move(self):		
 		if self.right == True: 
 			self.x += 2.5
 
@@ -401,7 +283,7 @@ class Enemy_7(object):
 		self.right = False
 		self.points = 50
 
-	def move(self, time):		
+	def move(self):		
 		if self.right == False: 	
 			self.x -= 2.5
 
@@ -429,46 +311,44 @@ class Enemy_7(object):
 			return None		
 
 class Enemy_8(object):
-	"""Moves in a square pattern at the top half of the screen clockwise."""
-	def __init__(self):		
+	"""Moves in a square pattern at the top half of the screen, either clockwise or
+		counter-clockwise depending on the starting point."""
+	def __init__(self,x):		
 		super(Enemy_8, self).__init__()
-		self.x = 550
+		self.x = x
 		self.y = 800
-		self.life = 60
+		self.life = 100
 		self.attack = "normal"
-		self.damage = 10
-		self.cooldown = 10
+		self.damage = 20
+		self.cooldown = 20
 		self.id = "hard_1"
 		self.right = False
 		self.down = True
 		self.points = 65
+		self.direction = 'counter-clockwise'
+		if x >= 300:
+			self.direction = 'clockwise'
 
-	def move(self, time):		
-		if self.y > 400 and self.down:
-			self.y -= 2.5					
-
-		if self.y == 400:
-			self.down = False
-
-		if not self.down and self.x > 50:
-			self.x -= 2.5
-		
-		if self.x == 50:
-			self.right = True
-
-		if self.right and self.y < 750:
-			self.y += 2.5
-
-		if self.y == 750:
-			self.down = True
-
-		if self.down and self.x < 550:
-			self.x += 2.5		
-
-		if self.x == 550:
-			self.right = False
-
-		return self						
+	def move(self):
+		if self.direction == 'counter-clockwise':
+			if self.x == 50 and self.y > 250:		###Moves downwards
+				self.y -= 2.5
+			if self.x < 550 and self.y == 250:		###Moves to the right
+				self.x += 2.5
+			if self.x == 550 and self.y < 750:		###Moves upwards
+				self.y += 2.5
+			elif self.x > 50 and self.y == 750:		###Moves to the left
+				self.x -= 2.5
+		elif self.direction == 'clockwise':
+			if self.x == 550 and self.y > 250: ###moves downwards
+				self.y -= 2.5					
+			if self.x > 50 and self.y == 250:		###Moves to the left
+				self.x -= 2.5
+			if self.x == 50 and self.y < 750:		###Moves upwards
+				self.y += 2.5
+			elif self.x < 550 and self.y == 750:		###Moves to the right
+				self.x += 2.5
+		return self
 
 	def shoot(self, time, player_point):
 		if time%10 == 0:
@@ -496,7 +376,7 @@ class Enemy_9(object):			#	  _|_|
 		self.down = False
 		self.points = 80
 
-	def move(self):	
+	def move(self):		
 		if self.y == 575 and self.x < 550 and not self.down:
 			self.x +=2.5
 
@@ -547,7 +427,7 @@ class Enemy_10(object):				#|_|_
 		self.down = False
 		self.points = 100
 
-	def move(self):	
+	def move(self):		
 		if self.y == 575 and self.x > 50 and not self.down:
 			self.x -=2.5
 
@@ -579,4 +459,4 @@ class Enemy_10(object):				#|_|_
 			bullet.obj_vx = (-self.x+player_point["x"])/player_point["y"]
 			return bullet
 		else:
-			return None					
+			return None
